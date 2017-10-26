82f6ecd8f950e18724c0e24909019aafcb981ef7
Vladimir Hodakov <vladimir@hodakov.me>
2017-10-26 18:20:50 +0400

    Removed ``dependencies.sh``.

--- dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go get github.com/go-sql-driver/mysql
-go get github.com/jmoiron/sqlx
-go get github.com/pressly/goose
-go get github.com/go-telegram-bot-api/telegram-bot-api
-go get gopkg.in/yaml.v2
